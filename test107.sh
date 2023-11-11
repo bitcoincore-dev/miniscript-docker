40c6b0671b2fc0f79b11242b7ce42ccf08b7e41c
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-11-10 20:12:58 -0500

    test107.sh

---
+++ test107.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+for i in {0..9}; do
+    echo $i
+    [  $i -gt 0 ] && echo "thresh(2,ltv:after($i),altv:after(1$i),a:pk(0))" | ./miniscript | jq
+    for j in {0..9}; do
+        echo $i $j
+        [  $i -gt 0 ] && [ $j -gt 0 ] && echo "thresh(2,ltv:after($i$j),altv:after(1$i$j),a:pk(0))" | ./miniscript | jq
+        for k in {0..9}; do
+            echo $i $j $k
+            echo "thresh(2,ltv:after(1$i$j$k$i$j$k$i$j$k),altv:after($j$k),a:pk(0))" | ./miniscript | jq
+        done
+    done
+done
+echo "thresh(2,ltv:after(1000000000),altv:after(100),a:pk(0))" | ./miniscript | jq
